27fc043ceef0212f47026cf142eadf9b8b9ab20d
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-04-17 17:30:46 +0100

    Added price to the views and controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -58,7 +58,7 @@ class ReviewsController < ApplicationController
   end
 
   def form_params
-    params.require(:review).permit(:title, :body, :score, :restaurant, :cuisine, :ambiance)
+    params.require(:review).permit(:title, :body, :score, :restaurant, :cuisine, :ambiance, :price)
   end
 
 end
