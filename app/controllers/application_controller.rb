4828e37d12689c04b5799a0372a1f3f343aa172f
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-04-25 14:52:27 +0100

    Adding helper methods to use in views. Refactoring code with "unless"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,12 @@ class ApplicationController < ActionController::Base
   # The find_current_cuser method wont work otherwise.
   before_action :find_current_cuser
 
+  # Add method to use in views.
+  helper_method :is_logged_in?
+
   # This is our own method (not convention)
   def find_current_cuser
-    if session[:user_id].present?
+    if is_logged_in?
       @current_user = User.find(session[:user_id])
     else
       @current_user = nil
@@ -16,13 +19,16 @@ class ApplicationController < ActionController::Base
 
   # Check login status
   def check_login
-    if session[:user_id].present?
-      # All good. Do nothing.
-    else
+    # unless can be used to replace an if/else when there is no action to take if the IF is true.
+    unless is_logged_in?
       redirect_to new_session_path
     end
   end
 
-
+  # Is the person logged in?
+  # By creating method, it can be used elsewhere to simplify the code
+  def is_logged_in?
+    session[:user_id].present?
+  end
 
 end
