c582d591832863da371761f69017236a2f303f0c
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-04-25 14:34:45 +0100

    Log-in/out links to app layout. Added if/else to app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Before any page loads, find the current user.
+  # The find_current_cuser method wont work otherwise.
+  before_action :find_current_cuser
+
+  # This is our own method (not convention)
+  def find_current_cuser
+    if session[:user_id].present?
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
+
+
 end
