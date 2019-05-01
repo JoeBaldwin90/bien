906fb085801c322d71cdf6d94b9ee0b645d7e07a
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-01 20:28:40 +0100

    Updated bookmarks ctlr. Added link for logged-in users to review page.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,18 @@
 class BookmarksController < ApplicationController
+
+  before_action :check_login
+
+  def create
+    # Find review we're bookmarking (can get this from `rails routes` in Terminal)
+    @review = Review.find(params[:review_id])
+
+    @bookmark = @review.bookmarks.new
+    @bookmark.user = @current_user
+
+    @bookmark.save
+
+    redirect_to review_path(@review)
+  end
+
+
 end
