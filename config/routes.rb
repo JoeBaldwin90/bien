37affd467338a20e0bc0a60b136499f921146129
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-01 20:14:02 +0100

    bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
 
   resources :reviews do
     resources :comments
+    resource :bookmark
   end
 
   # this allows us to use all of the methods "show/index/new/create" etc. run: $ rails routes
   resources :users
 
-  # Use singular as there can only be one session per user. One log in, one log out. 
+  # Use singular as there can only be one session per user. One log in, one log out.
   resource :session
 
   root "reviews#index"
