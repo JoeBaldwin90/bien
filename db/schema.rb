9f764b54f5325581301c81d94733af435a3cc4f7
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-03 15:32:37 +0100

    Added string column for photos to the reviews db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_210147) do
+ActiveRecord::Schema.define(version: 2019_05_03_142621) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "review_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_210147) do
     t.float "latitude"
     t.float "longitude"
     t.integer "user_id"
+    t.string "photo"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
