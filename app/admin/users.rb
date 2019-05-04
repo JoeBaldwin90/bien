7134c1e9800bfdecca40654380815c23c21ac6f5
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-04 11:58:55 +0100

    New Active Admin resource for User model.

---
+++ app/admin/users.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register User do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :username, :email, :real_name, :password, :password_confirmation, :is_admin
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+# From stackoverflow "active user to_param override"
+controller do
+    resources_configuration[:self][:finder] = :find_by_title
+end
+
+end
