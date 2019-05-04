e821f807444b712a3932de7c38007e241f95b037
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-04 11:51:14 +0100

    Permitted params for Active Admin Review tab

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -2,7 +2,8 @@ ActiveAdmin.register Review do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :body, :score, :restaurant,
+ :cuisine, :ambiance, :price, :address, :photo, :user_id
 #
 # or
 #
