a976e1634862cb90b01f980424b69a1074784c52
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-09 20:37:44 +0100

    Style updates

--- app/models/review.rb
+++ app/models/review.rb
@@ -20,6 +20,7 @@ class Review < ApplicationRecord
   validates :body, length: { minimum: 10 }
   validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :address, presence: true
+  validates :cuisine, presence: true
 
   def to_param
     id.to_s + "-" + title.parameterize #converts URL to readable string (SEO). Title is a method()
