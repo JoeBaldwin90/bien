4dddb51bfbc1c0e153dd93e4fb7fa7ab96f09af9
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-10-25 15:11:20 +0100

    Carrierwave config moved to credentials for security

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAZB2OREXTDUT6C46R',                     # required unless using use_iam_profile
-    aws_secret_access_key: '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', # required unless using use_iam_profile
+    aws_access_key_id:     Rails.application.credentials.aws_access_key,     # required unless using use_iam_profile
+    aws_secret_access_key: Rails.application.credentials.aws_secret_key, # required unless using use_iam_profile
     region:                'eu-west-2',
   }
-  config.fog_directory  = 'bien-reviews-2019'            # required
+  config.fog_directory  = Rails.application.credentials.fog_directory            # required
 end
