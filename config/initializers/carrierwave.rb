9ae9e7ab0e0501c20118a80638048d7dd2b67baf
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-10-25 14:52:07 +0100

    New AWS bucket and IAM credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAII6XL66VOECBL2ZQ',                     # required unless using use_iam_profile
-    aws_secret_access_key: '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', # required unless using use_iam_profile
+    aws_access_key_id:     'AKIAZB2OREXTDUT6C46R',                     # required unless using use_iam_profile
+    aws_secret_access_key: '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', # required unless using use_iam_profile
+    region:                'eu-west-2',
   }
-  config.fog_directory  = 'bien-reviews'                 # required
+  config.fog_directory  = 'bien-reviews-2019'            # required
 end
